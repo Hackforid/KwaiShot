bd5629f7de8351516b6e3d5942daa59bebec477b
kleist <zhouquanbest@gmail.com>
2018-06-13 09:08:09 +0800

    update

---
+++ app/src/main/java/com/smilehacker/kwaishot/ui/home/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.smilehacker.kwaishot.ui.home
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.smilehacker.kwaishot.R
+
+class HomeFragment : Fragment() {
+    private lateinit var mViewModel: HomeViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        println("onCreateView")
+        val view = inflater.inflate(R.layout.frg_home, container, false)
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+    }
+}
---
+++ app/src/main/java/com/smilehacker/kwaishot/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.smilehacker.kwaishot.ui.home
+
+import android.arch.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+
+}
