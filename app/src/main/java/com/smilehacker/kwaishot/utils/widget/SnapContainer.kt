24e8300978c3d7f2aa634802ecef295033485b4b
kleist <zhouquanbest@gmail.com>
2018-06-24 16:55:14 +0800

    use draghelper

--- app/src/main/java/com/smilehacker/kwaishot/utils/widget/SnapContainer.kt
+++ app/src/main/java/com/smilehacker/kwaishot/utils/widget/SnapContainer.kt
@@ -1,7 +1,7 @@
 package com.smilehacker.kwaishot.utils.widget
 
 import android.content.Context
-import android.support.v4.view.ViewCompat
+import android.support.v4.widget.ViewDragHelper
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -13,36 +13,48 @@ import com.smilehacker.kwaishot.utils.DLog
  */
 class SnapContainer : FrameLayout {
 
+    private val mDragHelper = ViewDragHelper.create(this, 1.0f, DragCallback())
 
     private var mDraggingDown = false
     private var mDraggingUp = false
+
+    private lateinit var mPrimaryChild: View
 
     constructor(context: Context) : this(context, null, 0)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-
-    override fun onStartNestedScroll(child: View?, target: View?, nestedScrollAxes: Int): Boolean {
-        DLog.i("onStartNestedScroll")
-        return (nestedScrollAxes or ViewCompat.SCROLL_AXIS_VERTICAL) != 0
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        mPrimaryChild = getChildAt(0)
     }
 
-    override fun onNestedPreScroll(target: View?, dx: Int, dy: Int, consumed: IntArray?) {
-        DLog.i("onNestedPreScroll $target $dx $dy $consumed")
-        super.onNestedPreScroll(target, dx, dy, consumed)
+    inner class DragCallback: ViewDragHelper.Callback() {
+        override fun tryCaptureView(child: View, pointerId: Int): Boolean {
+            DLog.d("tryCaptureView $child $pointerId")
+            return child == mPrimaryChild
+        }
+        override fun clampViewPositionVertical(child: View, top: Int, dy: Int): Int {
+            DLog.d("clampViewPositionVertical $child $top $dy")
+            return top
+        }
     }
 
-    override fun onNestedScroll(target: View?, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int) {
-        DLog.i("onNestedScroll $target $dxConsumed $dyConsumed $dxUnconsumed $dyUnconsumed")
-        super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed)
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+
+        val action = ev.action
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            mDragHelper.cancel()
+            return false
+        }
+
+        return mDragHelper.shouldInterceptTouchEvent(ev)
     }
 
-    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
-        return super.onInterceptTouchEvent(ev)
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        mDragHelper.processTouchEvent(event)
+        return true
     }
 
-    override fun onStopNestedScroll(child: View?) {
-        super.onStopNestedScroll(child)
-    }
 
 }
